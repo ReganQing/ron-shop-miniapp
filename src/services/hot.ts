2ca5a2d8b4134f116dabfe05afff60511bc8c8d0
ReganQing <1814047158@qq.com>
2023-11-22 13:23:03 +0800

    feat 热门推荐类型定义

--- src/services/hot.ts
+++ src/services/hot.ts
@@ -1,4 +1,5 @@
 import type { PageParams } from '@/types/global'
+import type { HotResult } from '@/types/hot'
 import { http } from '@/utils/http'
 
 type HotParams = PageParams & { subtype?: string }
@@ -9,7 +10,7 @@ type HotParams = PageParams & { subtype?: string }
  * @returns 热门推荐数据集合
  */
 export const getHotRecommendAPI = (url: string, data?: HotParams) => {
-  return http({
+  return http<HotResult>({
     method: 'GET',
     url,
     data,
