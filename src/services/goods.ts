cbcd7f87f8543351ef29c7097cfe1eca2a7feb33
ReganQing <1814047158@qq.com>
2023-11-28 13:27:16 +0800

    feat 商品详情准备工作

---
+++ src/services/goods.ts
@@ -0,0 +1,16 @@
+import { http } from '@/utils/http'
+
+/**
+ * 商品详情
+ * @param id 商品id
+ * @returns 商品详情信息
+ */
+export const getGoodsByIdAPI = (id: string) => {
+  return http({
+    method: 'GET',
+    url: '/goods',
+    data: {
+      id,
+    },
+  })
+}
