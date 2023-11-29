46ebcb29cd163f09cdf736ead7310425b2ff1886
ReganQing <1814047158@qq.com>
2023-11-29 15:59:17 +0800

    feat 修改地址模块的数据回显

--- src/pagesMember/address-form/address-form.vue
+++ src/pagesMember/address-form/address-form.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { postMemberAddressAPI } from '@/services/address'
+import { getMemberAddressByIdAPI, postMemberAddressAPI } from '@/services/address'
+import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 
 // 表单数据
@@ -50,6 +51,21 @@ const onSubmit = () => {
 const query = defineProps<{
   id?: string
 }>()
+
+// 获取收货地址详情
+const getMemberAddressByIdData = async () => {
+  if (query.id) {
+    // 发送请求
+    const res = await getMemberAddressByIdAPI(query.id)
+    // 把数据合并到表单中
+    Object.assign(form.value, res.result)
+  }
+}
+
+// 加载页面
+onLoad(() => {
+  getMemberAddressByIdData()
+})
 
 // 动态设置标题
 uni.setNavigationBarTitle({ title: query.id ? '修改地址' : '新建地址' })
--- src/services/address.ts
+++ src/services/address.ts
@@ -14,9 +14,25 @@ export const postMemberAddressAPI = (data: AddressParams) => {
   })
 }
 
+/**
+ * 获取收货地址列表
+ * @returns 收货地址列表
+ */
 export const getMemberAddressAPI = () => {
   return http<AddressItem[]>({
     method: 'GET',
     url: '/member/address',
+  })
+}
+
+/**
+ * 获取收货地址详情
+ * @param id 地址id(路径参数)
+ * @returns 收货地址详情
+ */
+export const getMemberAddressByIdAPI = (id: string) => {
+  return http<AddressItem>({
+    method: 'GET',
+    url: `/member/address/${id}`,
   })
 }
