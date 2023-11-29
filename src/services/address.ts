d6c9eecb8f4c2925cb933e8c84a6aa1d7c85870e
ReganQing <1814047158@qq.com>
2023-11-29 15:14:11 +0800

    feat 地址模块-新建收货地址

---
+++ src/services/address.ts
@@ -0,0 +1,15 @@
+import type { AddressParams } from '@/types/address'
+import { http } from '@/utils/http'
+
+/**
+ * 添加收货地址
+ * @param data 地址参数
+ * @returns 操作结果
+ */
+export const postMemberAddressAPI = (data: AddressParams) => {
+  return http({
+    method: 'POST',
+    url: '/member/address',
+    data,
+  })
+}
